15dc19dc75864dfe67964a60ead3a701a8040b31
João Vitor Alves Barradas <20171ENEL0330@cefetes.br>
2019-08-16 17:48:28 -0300

    aula2

--- Acomp2/SelectionSort.cs
+++ Acomp2/SelectionSort.cs
@@ -2,9 +2,28 @@
 {
     class SelectionSort : ISort
     {
-        public void Sort(int[] E, int n)
-        {
-            throw new System.NotImplementedException();
-        }
+            public void Sort(int[] E, int n)
+            {
+                int k, x;
+                for (int i = 0; i < n - 1; i++) //(1)
+                {
+                    k = i;
+                    x = E[i];
+                    for (int j = i + 1; j < n; j++) //(2)
+                    {
+                        if (E[j] < x)
+                        {
+                            k = j;
+                            x = E[k];
+                        }
+
+
+                    }
+                    E[k] = E[i];
+                    E[i] = x;
+                }
+
+            }
+        
     }
 }
